b1c25ab11629f5e1910a12a508818500daee27d6
Wuyilingwei <53390868+wuyilingwei@users.noreply.github.com>
2026-08-14 04:07:18 +0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,11 +6,15 @@ import { resolve } from 'node:path';
 export default defineConfig({
     publicDir: 'static',
     plugins: [vue(), cloudflare()],
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(import.meta.dirname, 'index.html'),
-                admin: resolve(import.meta.dirname, 'admin/index.html'),
+    environments: {
+        client: {
+            build: {
+                rollupOptions: {
+                    input: {
+                        main: resolve(import.meta.dirname, 'index.html'),
+                        admin: resolve(import.meta.dirname, 'admin/index.html'),
+                    },
+                },
             },
         },
     },
